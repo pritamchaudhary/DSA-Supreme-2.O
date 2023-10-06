faeba3a8ea91e2cf0d47c8663256ef7604a52a02
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2023-10-06 13:17:57 +0530

    week 6 class1 completed

---
+++ Week 6 Basic Maths and Pointers/class 1/basic_pointer.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+//   char ch[10] = "babbar";
+//   cout << &ch[0];
+//   char* cptr = &ch;
+
+//   char ch[10] = "a";
+//   char *cptr = ch;
+//   cout << cptr;
+
+
+//   int arr[5] = {1,2,3,4,5};
+//   arr = arr + 1;
+
+
+
+
+//   int a = 5;
+//   int* p = &a;
+
+//   p = p + 1;
+//   cout << *p;
+//   int* ptr = 0;
+//   cout << *ptr << endl;
+
+
+//   int a = 5;
+//   cout << a << endl;
+//   cout << "address of a : "<< &a << endl;
+  
+
+//   //pointer creation
+//   int* ptr = &a;
+//   //acessing value stored at address stored in ptr
+//   cout << ptr << endl;
+//   cout << "Accessing: " << *ptr << endl;
+//   cout << &ptr << endl;
+
+
+  return 0;
+}
