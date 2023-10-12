32c5dd25c7f70186096bb041e07739206574f4d1
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2023-10-12 19:55:57 +0530

    week5 assignment complete

---
+++ Week 5 char Arrays and strings/Assignment/findTheIndexOfTheFirstOccurrenceInAString.cpp
@@ -0,0 +1,25 @@
+// Leetcode 28. Find the Index of the First Occurrence in a String
+
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.size();
+        int m = needle.size();
+        for(int i=0; i<=n-m; i++){
+            for(int j=0; j<m; j++){
+                if(needle[j] != haystack[i+j]){
+                    break;
+                }
+                if(j == m-1){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+};
