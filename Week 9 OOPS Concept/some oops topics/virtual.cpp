68e2ab6bb63d25108f86f8abb3c71d4e6e8b723f
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2023-11-03 22:36:37 +0530

    add virtual

---
+++ Week 9 OOPS Concept/some oops topics/virtual.cpp
@@ -0,0 +1,44 @@
+// virtual
+
+#include <iostream>
+using namespace std;
+
+class Base
+{
+public:
+    Base()
+    {
+        cout << "Base ctor \n";
+    }
+
+    virtual ~Base()
+    {
+        cout << "Base dtor \n";
+    }
+};
+
+class Derived : public Base
+{
+    int *a;
+
+public:
+    Derived()
+    {
+        a = new int[1000];
+        cout << "Derived ctor \n";
+    }
+
+    ~Derived()
+    {
+        delete[] a;
+        cout << "Derived dtor\n";
+    }
+};
+
+int main()
+{
+    Base *b = new Derived();
+    delete b;
+
+    return 0;
+}
